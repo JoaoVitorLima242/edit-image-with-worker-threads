a7b5494b4f2bbd34651f49d9f24f6294a00ead1f
JoaoVitorLima242 <jlima0500@gmail.com>
2024-05-11 16:41:22 -0300

    starting app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+
+
+// https://upload.wikimedia.org/wikipedia/pt/d/df/Sonic_1991.png
+// https://mb.srb2.org/attachments/s3sonic_css_20240105165802-png.108841/
+//
+// background
+// https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAYYH1hrQdhvHUE42suisZf1PwMNnqyCtKpdWArn7VJQ&s
